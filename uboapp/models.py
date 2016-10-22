4ba7e4a847bffb9487f9eef2a6820271cd8f80b5
ugo <ugo@croissantmail.com>
2016-10-22 11:52:14 +0900

    1st commit

--- uboapp/models.py
+++ uboapp/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Building(models.Model):
-    address = models.CharField(max_length=200)
-    zipcode = models.CharField(max_length=5)
+    streetname = models.CharField(max_length=200, default=0)
+    streetnum = models.CharField(max_length=5, default=0)
+    zipcode = models.CharField(max_length=5, default=0)
+    latitude = models.CharField(max_length=10, default=0)
+    longitude = models.CharField(max_length=10, default=0)
 
 
 def __str__(self):
-    return self.address
+    return self.streetname
--- uboapp/views.py
+++ uboapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Building
 
 # Create your views here.
+
+def building_list(request):
+    buildings = Building.objects.all()
+    return render(request, 'uboapp/building_list.html', {'buildings': buildings})
